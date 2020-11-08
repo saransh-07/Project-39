6ebec41d1bca83cd9a58c95250b29e56dc08a949
saransh-07 <69812136+saransh-07@users.noreply.github.com>
2020-11-08 21:45:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,13 +13,13 @@ var player_img;
 var disPos = 30;
 
 function preload(){
-  back_img = loadImage("images/jungle.jpg");
-  player_img = loadImage("images/basket2.png");
-  fruit1_img = loadImage("images/apple2.png");
-  fruit2_img = loadImage("images/banana2.png");
-  fruit3_img = loadImage("images/melon2.png");
-  fruit4_img = loadImage("images/orange2.png");
-  fruit5_img = loadImage("images/pineapple2.png");
+  back_img = loadImage("../images/jungle.jpg");
+  player_img = loadImage("../images/basket2.png");
+  fruit1_img = loadImage("../images/apple2.png");
+  fruit2_img = loadImage("../images/banana2.png");
+  fruit3_img = loadImage("../images/melon2.png");
+  fruit4_img = loadImage("../images/orange2.png");
+  fruit5_img = loadImage("../images/pineapple2.png");
   fruitArray = [];
 }
 function setup() {
